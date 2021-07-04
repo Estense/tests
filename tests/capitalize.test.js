043ca48ca31f52ff73f27cb0d4f77a67eb058196
Andrey Vernov <a.vernov16@gmail.com>
2021-07-04 13:53:36 +0500

    jest

--- tests/capitalize.test.js
+++
@@ -1,7 +0,0 @@
-import { capitalize } from '../src/capitalize.js';
-import assert from 'assert';
-
-assert.strictEqual(capitalize('hello'), 'Hello')
-assert.strictEqual(capitalize(''), '')
-
-console.log('Все тесты пройдены!');
